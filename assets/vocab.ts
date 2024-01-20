74433fb1930bbca54b470a41cf0bbb3746946133
Dean C <dcavaliere@arborprousa.com>
2024-01-20 16:18:58 -0500

    Auto-generated commit [ci skip]

---
+++ assets/vocab.ts
@@ -0,0 +1,49 @@
+export class Vocab {
+  testList: string[] =[
+    '1', '2', '3'
+  ];
+  vocabList: string[] = [
+    "Idiot.",
+    "Buffoon.",
+    "Ass.",
+    "Fool.",
+    "Dolt.",
+    "Nincompoop.",
+    "BlockHead.",
+    "Dunce.",
+    "Ignoramus.",
+    "Dullard.",
+    "Moron.",
+    "Simpleton.",
+    "Donkey.",
+    "Jackass.",
+    "Chump.",
+    "Numbskull.",
+    "Dope.",
+    "Nitwit.",
+    "Bonehead.",
+    "Birdbrain.",
+    "Twerp.",
+    "Dork.",
+    "Jerk.",
+    "Imbecile.",
+    "Loser.",
+    "Oaf.",
+    "Goon.",
+    "Barbarian.",
+    "Brute.",
+    "Stooge.",
+    "Lamebrain.",
+    "Sucker.",
+    "Loony.",
+    "Nerd.",
+    "Dummy.",
+    "Dumdum.",
+    "Gremlin.",
+    "Goblin.",
+    "Idiot Sandwich.",
+    "Doo Doo Face",
+
+
+  ];
+}
